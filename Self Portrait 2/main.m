ee00073d4705ca673db304cac1792497f174a8d0
Daniel Kwolek <Daniel@daniels-macbook-pro.local>
2016-08-16 18:09:28 -0400

    Initial Commit

---
+++ Self Portrait 2/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Self Portrait 2
+//
+//  Created by Daniel Kwolek on 8/16/16.
+//  Copyright © 2016 Arcore. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
